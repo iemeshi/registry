84ec5fc4151ccf460a8fd1596f3d4e793621f1b4
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2020-05-05 13:13:54 +0900

    Add test

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
-const apps = require('./apps.json')
+const apps = require("./apps.json");
 
 for (const app of apps) {
-  if(!app['都道府県名'] || !app['地域名'] || !app['サブドメイン']　|| !app['GitHub']) {
-    throw new Error('不足しているパラメータがあります。')
+  if (
+    !app["都道府県名"] ||
+    !app["地域名"] ||
+    !app["サブドメイン"] ||
+    !app["GitHub"]
+  ) {
+    throw new Error("不足しているパラメータがあります。");
+  }
+  const subdomain = app["サブドメイン"];
+  if (
+    subdomain[0] === "-" ||
+    subdomain[subdomain.length - 1] === "-" ||
+    subdomain.indexOf("--") !== -1 ||
+    !/[a-z0-9-]{1,63}$/.test(subdomain)
+  ) {
+    throw new Error("このサブドメインは使用できません。");
   }
 }
 
-process.stdout.write('success!')
+process.stdout.write("success!");
